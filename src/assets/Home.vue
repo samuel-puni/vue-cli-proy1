77c89cf673cce16b9e38edd9bb38b40385c14aaa
samuel-puni <samuel.puni@gmail.com>
2019-05-10 19:07:36 -0400

    Componentes

---
+++ src/assets/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Service Staus:  {{ status}}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: { 
+            changeStatus() {
+                this.status = 'Normal'
+            }
+        },
+    }
+</script>
